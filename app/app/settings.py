c30a45710330c02031a35cd1224942d6ebcc45f0
alhilaladamu <alhilaladam@gmail.com>
2020-09-26 09:48:14 -0700

    Updated Travis and Flake8

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'djangorestframework',
-    #'Knox-restframework',
+    
 ]
 
 MIDDLEWARE = [
